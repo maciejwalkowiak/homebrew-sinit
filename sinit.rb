39201278c4220f18b89925e0ca946e4c81ea4d8d
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-10-30 14:49:01 +0100

    fix sha

--- sinit.rb
+++ sinit.rb
@@ -2,7 +2,7 @@ class Sinit < Formula
   desc "Shiny Spring Boot project initializer"
   homepage "https://github.com/maciejwalkowiak/sinit"
   url "https://github.com/maciejwalkowiak/sinit/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7dbe492a76df3402406efcda9f992be3a2fd71ebabeaac4e65c52e9eafdb287d"
+  sha256 "811a1532ac0a8a28ec83279300858725d7f46ec1ae9390f0ff6a9f2a2e5dde7d"
   license "MIT"
 
   depends_on "gum"
